39ee3853a8b1bbc53189d94f1e9fa50ecbc2ae7c
zeynepbozkirr <bozkrzeynep@gmail.com>
2022-11-27 10:51:34 +0300

    card comp change responsive

--- components/Card.js
+++ components/Card.js
@@ -2,38 +2,45 @@ import React, { useEffect, useState } from "react";
 import { Button, Card, Col, Input, Row } from "antd";
 import { useCollection } from "../Hooks/useCollection";
 import Link from "next/link";
+import { Typography } from "antd";
+import { space } from "@chakra-ui/react";
+const { Paragraph } = Typography;
 
 const CardComp = () => {
   const { documents: Posts } = useCollection("Posts");
+  const [ellipsis, setEllipsis] = useState(true);
 
   return (
     <>
-      <Row>
+      <Row justify="space-around">
         {Posts && Posts?.length !== 0
           ? Posts.map((post) => (
-              <Col span={6}>
-                <Card
-                  size="small"
-                  title={post.Title}
-                  extra={<Link href={`/posts/${post.id}`}>more</Link>}
-                  style={{
-                    width: 300,
-                    height: 300,
-                    overflow: "hidden",
-                    background: "#EED6C4",
-                    marginBottom: "10px",
-                  }}
-                >
-                  <p
+              <Row>
+                <Col>
+                  <Card
+                    size="small"
+                    title={post.Title}
+                    extra={<Link href={`/posts/${post.id}`}>more</Link>}
                     style={{
                       width: 300,
                       height: 300,
+                      overflow: "hidden",
+                      background: "#EED6C4",
+                      marginBottom: "10px",
                     }}
                   >
-                    {post.Text}
-                  </p>
-                </Card>
-              </Col>
+                    <Paragraph
+                      ellipsis={{
+                        rows: 6,
+                        expandable: false,
+                        symbol: "more",
+                      }}
+                    >
+                      {post.Text}
+                    </Paragraph>
+                  </Card>
+                </Col>
+              </Row>
             ))
           : null}
       </Row>
