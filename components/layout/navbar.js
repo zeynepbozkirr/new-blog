a0c51c397c12c268e8cd4eb523d33a2ae36e8b7e
zeynepbozkirr <bozkrzeynep@gmail.com>
2022-12-23 18:04:38 +0300

    finished

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -4,16 +4,18 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <Row justify="space-around" style={{ background: "#E2E3E4", height: 55 }}>
+    <Row
+      style={{
+        background: "#E2E3E4",
+        height: 55,
+      }}
+    >
       <Col span={12}>
         <Link href="/">
           <text className="Navbar">Bekir Yetim</text>
         </Link>
       </Col>
-      <Col
-        span={12}
-        style={{ display: "flex", justifyContent: "end", paddingRight: "30px" }}
-      >
+      <Col span={6} offset={6}>
         <Link href="/About">
           <text className="Navbar">About </text>
         </Link>
